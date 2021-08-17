75e8aae25c68796f06d72d2425a59ca0180d7a91
Ruslan <ruslan-skribl1998@mail.ru>
2021-08-17 18:50:56 +0300

    fix again trouble with code

--- libs/pymysklad/pymyskald.py
+++ libs/pymysklad/pymyskald.py
@@ -184,7 +184,7 @@ class MSDict:
             r = requests.get(f'{self.URL}?limit={batch_size}&offset={total}',
                              headers={'Authorization': self.auth})
             response_data = r.json()
-            codes += [item['code'] for item in response_data.get('rows', [])]
+            codes += [item['code'] for item in list(filter(lambda item: 'code' in item, response_data.get('rows', [])))]
             total += batch_size
 
         return codes
