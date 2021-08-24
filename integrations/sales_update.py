973846ae4a6b86ecc48a197b84f5c11ac3699ff4
Ruslan Talypov <52674752+skribl5000@users.noreply.github.com>
2021-08-24 06:20:27 -0400

    Update sales_update.py

--- integrations/sales_update.py
+++ integrations/sales_update.py
@@ -131,7 +131,7 @@ for store in df_sales['warehouseName'].unique():
         else:
             r = None
 
-        if r.status_code != 200:
+        if r.status_code != 200 or r is None:
             error_barcodes.add(row['barcode'])
 
 print('Barcodes was not found:')
